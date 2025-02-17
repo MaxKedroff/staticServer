9ef45b1261518981b78ecfbf0bbd6831b119e232
MaxKedroff <kedrov.maksim12345@gmail.com>
2025-02-17 09:35:05 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace staticServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int readerTasks = 5;
+            const int writerTasks = 5;
+
+            const int writeIterations = 50;
+
+            const int readIterations = 100;
+
+            Task[] tasks = new Task[readerTasks + writerTasks];
+
+            for (int i = 0; i < readerTasks; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    for (int j = 0; j < readIterations; j++)
+                    {
+                        int currentCount = Server.GetCount();
+                        Console.WriteLine($"Чтение: {currentCount}");
+                        Thread.Sleep(10);
+                    }
+                });
+            }
+
+            for (int i = 0; i < writerTasks; i++)
+            {
+                tasks[readerTasks + i] = Task.Run(() =>
+                {
+                    for (int j = 0; j < writeIterations; j++)
+                    {
+                        Server.AddToCount(1);
+                        Console.WriteLine("Запись: +1");
+                        // Имитация задержки
+                        Thread.Sleep(20);
+                    }
+                });
+            }
+
+            Task.WaitAll();
+
+            int expected = writerTasks * writeIterations;
+            int finalCount = Server.GetCount();
+            Console.WriteLine($"\nИтоговое значение: {finalCount} (ожидалось {expected})");
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace staticServer
+{
+    public static class Server
+    {
+        private static int _count = 0;
+        private static readonly ReaderWriterLockSlim _rwLock = new ReaderWriterLockSlim();
+
+
+        public static int GetCount()
+        {
+            _rwLock.EnterReadLock();
+            try
+            {
+                return _count;
+            }
+            finally
+            {
+                _rwLock.ExitReadLock();
+            }
+        }
+
+
+        public static void AddToCount(int value)
+        {
+            _rwLock.EnterWriteLock();
+            try
+            {
+                _count += value;
+            }
+            finally
+            {
+                _rwLock.ExitWriteLock();
+            }
+        } 
+    }
+}
